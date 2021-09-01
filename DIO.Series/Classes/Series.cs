2bd266319907b84695edb9e728918a45e9f692d7
Gabriel1604 <gabrieloliveira005160@gmail.com>
2021-08-31 22:04:18 -0300

    Primeiro commit do projeto

---
+++ DIO.Series/Classes/Series.cs
@@ -0,0 +1,72 @@
+using System;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+        private bool Favorito { get; set; }
+
+        //Metodos
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+            this.Favorito = false;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido + Environment.NewLine;
+            retorno += "Favorito: " + this.Favorito;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public bool retornaFavorito()
+        {
+            return this.Favorito;
+        }
+
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        public void Favoritar()
+        {
+            this.Favorito = true;
+        }
+
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Projeto de cadastro simples de série :computer: 
+
+Olá pessoal, esse foi meu primeiro projeto em .Net, onde foi criado um programa simples para cadastrar séries, nesse programa é possível:
+
+- Listar as séries cadastradas
+- Cadastrar novas séries
+- Atualizar séries
+- Favoritar série
+- Excluir série
+- Visualizar a série
+
+Espero que gostem e provavelmente de acordo com meu aprendizado eu vou seguir atualizando esse projeto.
